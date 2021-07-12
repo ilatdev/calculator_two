eb28706f47878aa1b062dc270555d8369bcafb90
Ian Latour <latourian@gmail.com>
2021-07-12 17:55:59 -0300

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg'
 import './App.css'
+import Calculator from './Calculator'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Calculator />
+      </header>
     </div>
   )
 }
