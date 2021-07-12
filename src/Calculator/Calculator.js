d51950c2082a62f825202ae627c1953857c5882b
Ian Latour <latourian@gmail.com>
2021-07-12 17:57:58 -0300

    add calculator main file

---
+++ src/Calculator/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <div>
+      <div className="inputPanel">
+        <h1>Calculadora</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
