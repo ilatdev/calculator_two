ca67c2a16ab5ac284db6288603ebe7766667aff3
Ian Latour <latourian@gmail.com>
2021-07-15 14:51:11 -0300

    added drag and drop list

--- src/Calculator/ContainerTable/components/ViewRow.js
+++ src/Calculator/ContainerTable/components/ViewRow.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function ViewRow({ leyend, value }) {
+function ViewRow(props) {
   return (
     <div className="viewRow">
-      <div className="viewRow-leyend">{leyend}</div>
-      <div className="viewRow-value">{value}</div>
+      <div className="viewRow-leyend">{props.leyend}</div>
+      <div className="viewRow-value">{props.value}</div>
     </div>
   )
 }
--- src/Calculator/ContainerTable/components/ViewTable.js
+++ src/Calculator/ContainerTable/components/ViewTable.js
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ViewRow from './ViewRow'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list)
+  const [removed] = result.splice(startIndex, 1)
+  result.splice(endIndex, 0, removed)
+
+  return result
+}
 
 function ViewTable(props) {
+  const [dragAndDrop, setDragAndDrop] = useState(null)
+
+  useEffect(() => {
+    setDragAndDrop(Object.entries(props))
+    // Bug a resolver
+    // Al cambiar props pierde orden previo del drag and drop
+  }, [props])
+
+  const onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return
+    }
+    const items = reorder(
+      dragAndDrop,
+      result.source.index,
+      result.destination.index
+    )
+    return setDragAndDrop(items)
+  }
+
   return (
     <div className="viewTable">
-      {Object.keys(props).map((key, index) => (
-        <ViewRow key={index} leyend={key} value={props[key]} />
-      ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {!!dragAndDrop &&
+                dragAndDrop.map((item, index) => (
+                  <Draggable
+                    key={`${index}`}
+                    draggableId={`${index}`}
+                    index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        className={snapshot.isDragging ? 'isDragging' : ''}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}>
+                        <ViewRow leyend={item[0]} value={item[1]} />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   )
 }
